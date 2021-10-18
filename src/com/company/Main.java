aecee5f48dff11af5c76a3a95417f5946e280b1f
PVicoria23 <pvv230882@gmail.com>
2021-10-18 14:17:55 +0300

    added arithmetic operations.

---
+++ src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        double w = 10.0 / 4;
+        System.out.println(w);
+
+        int a = 33;
+        int b = 5;
+        int c = a % b;
+        int d = 22 % 4;
+        System.out.println(c);
+        System.out.println(d);
+
+        int t =5;
+        int y =++t;
+        System.out.println(t);
+        System.out.println(y);
+
+        int q = 10;
+        int e= q++;
+        System.out.println(q);
+        System.out.println(e);
+
+        int r = 8;
+        int u = --r;
+        System.out.println(r);
+        System.out.println(u);
+
+        int p = 14;
+        int x = p--;
+        System.out.println(p);
+        System.out.println(x);
+
+        int v = 8;
+        int n = 7;
+        int m = v + 5 * ++n;
+        System.out.println(m);
+
+        int z = 10;
+        int o = 4;
+        boolean s = z == o;
+        boolean f = z == 10;
+        System.out.println(s);
+        System.out.println(f);
+        
+
+
+
+
+
+
+
+    }
+}
