05887d22f21e04365df2e91ab23b24391d19f0f3
PVicoria23 <pvv230882@gmail.com>
2021-10-18 12:53:17 +0300

    added arithmetic operations.

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -12,8 +12,36 @@ public class Program {
         System.out.println(d);
         int t=35;
         System.out.println(35);
+        final int LIMIT =12;
+        System.out.println(LIMIT);
 
+        int r = 10;
+        int p = 7;
+        int c = r + p;
+        int n = 4 + p;
+        System.out.println(c);
+        System.out.println(n);
 
+        int i= 10;
+        int o = 7;
+        int s = i - o;
+        int f = 4 - i;
+        System.out.println(s);
+        System.out.println(f);
+
+        int g= 10;
+        int h = 7;
+        int j = a * h;
+        int k = h * 5;
+        System.out.println(j);
+        System.out.println(k);
+
+        int l= 20;
+        int z = 5;
+        int v = l / z;
+        double m = 22.5 / 4.5;
+        System.out.println(v);
+        System.out.println(m);
 
 
     }
